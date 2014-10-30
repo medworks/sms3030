4a1f38151b4da8b62be9d656a3024d57923271c7
mojiAm <amjadi.mojtaba@gmail.com>
2014-10-30 17:19:33 +0330

    seprate files

---
+++ inc/header.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+	<!-- Basic -->
+	<meta charset="utf-8">
+	<title>SMS3030.ir</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="author" content="Mediateq.ir">  
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Theme CSS - Skin Version -->
+	<link href="./css/style.css" rel="stylesheet" media="screen" class="skin_version">
+	<!-- Responsive CSS -->
+	<link href="./css/theme-responsive.css" rel="stylesheet" media="screen">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="./favicon.ico">
+	<link rel="apple-touch-icon" href="./apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="./apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="./apple-touch-icon-114x114.png">  
+
+	<!-- Head Libs -->
+	<script src="./js/modernizr.js"></script>
+
+	<!--[if IE]>
+	  <link rel="stylesheet" href="./css/ie.css">
+	<![endif]-->
+
+	<!--[if lte IE 8]>
+	  <script src="./js/html5shiv.js"></script>
+	  <script src="./js/respond.js"></script>
+	<![endif]-->
+
+	<!-- Skins Changer-->
+	<script type="text/javascript" src="http://www.google.com/jsapi"></script> 
+</head>
+<body> 
+	<!-- layout-->
+	<div id="layout">
+		<!-- Login Client -->
+		<div class="jBar">
+		  <div class="container">            
+		      <div class="row rtl">                
+		          <!-- <div class="col-md-offset-2 col-md-4">
+		              <h1>Client Login</h1>
+		              <form action="" method="post">
+		                <input value="23f8a1560aed4d47eca5b5032e008ed7764a9426" name="token" type="hidden">
+		                <input name="username" placeholder="demo@iwthemes.com" required="" type="email">
+		                <input kl_virtual_keyboard_secure_input="on" name="password" placeholder="demo" required="" type="password">
+		                <input class="button" value="Sign in" type="submit">
+		              </form>
+		          </div> -->
+
+		          <div class="col-md-12 contact_info">
+		              <h1>تماس با ما</h1>
+		              <ul>
+		                  <li><span>شماره تماس: </span>98-51-766-6436</li>
+		                  <li><span>آدرس ایمیل: </span><a href="#">info@mediateq.ir</a></li>
+		                  <li><span>آدرس پستی:</span>مشهد، سه راه فلسطین، ساختمان 202</li>
+		              </ul>                    
+		          </div>                
+		          <p class="jTrigger downarrow">بستن صفحه</p>
+		      </div>
+		  </div>
+		</div>
+		<span class="jRibbon jTrigger up">تماس با ما</span>
+		<div class="line"></div>
+		<!-- End Login Client -->
+		<!-- Header -->
+		<header>
+		  <!-- Info Head -->
+		  <section class="container info_head">      
+		      <ul>  
+		          <li class="ltr"><i class="fa fa-headphones"></i><a href="#">+985137666436</a></li>
+		          <li class="ltr"><i class="fa fa-fax"></i><a href="#">+985137613679</a></li>
+		      </ul>         
+		  </section>
+		  <!-- Info Head -->
+		  <!-- Nav -->
+		  <nav class="gray">
+		      <div class="container">
+		          <div class="row">
+		            <!-- Logo -->
+		            <div class="logo col-md-2">
+		                <span class="bg_logo"></span>
+		                <h1>SMS <span>3030</span></h1>
+		            </div>
+		            <!-- End Logo -->
+
+		            <div class="col-md-10">
+		                <!-- Menu-->
+		                <ul id="menu" class="sf-menu">
+		                    <li class="current"><a href="./">صفحه اصلی</a></li>
+		                    <li>
+		                        <a href="#">ورود به سامانه</a>
+		                        <!-- <ul>                                  
+		                          <li><a href="shared_hosting.html">Shared Hosting</a></li>
+		                          <li><a href="virtual_servers.html">Virtual Servers</a></li>
+		                          <li><a href="dedicated_servers.html">Dedicated Servers</a></li>
+		                        </ul> -->
+		                    </li>  
+		                    <li>
+		                         <a href="#">پنل ها</a>
+		                        <!-- <ul>
+		                            <li>
+		                                <a href="portfolio-2-columns.html">Portfolio 2 Columns</a>
+		                            </li>
+		                            <li>
+		                                <a href="portfolio-3-columns.html">Portfolio 3 Columns</a>
+		                            </li>
+		                            <li>
+		                                <a href="portfolio-4-columns.html">Portfolio 4 Columns</a>
+		                            </li>
+		                        </ul> -->
+		                    </li>
+		                    <li>
+		                        <a href="#">امکانات</a>
+		                        <!-- <ul> 
+		                            <li>
+		                                <a href="#">Pages</a>
+		                                <ul>                            
+		                                    <li><a href="page-full-width.html">Full Width</a></li>
+		                                    <li><a href="page-left-sidebar.html">Left Sidebar</a></li>
+		                                    <li><a href="page-right-sidebar.html">Right Sidebar</a></li>
+		                                    <li><a href="page-faq.html">FAQ</a></li>
+		                                    <li><a href="sitemap.html">Sitemap</a></li>
+		                                 </ul>
+		                            </li> 
+		                            <li>
+		                                <a href="#">Error Pages</a>
+		                                <ul>                                  
+		                                  <li><a href="403_page.html">403 Page</a></li>
+		                                  <li><a href="404_page.html">404 Page</a></li>
+		                                  <li><a href="405_page.html">405 Page</a></li>
+		                                  <li><a href="500_page.html">500 Page</a></li>
+		                                  <li><a href="503_page.html">503 Page</a></li>
+		                                  <li><a href="offline.html">Website is Offline</a></li>
+		                                </ul>
+		                            </li>  
+		                            <li><a href="feature-grid-system.html">Grind System</a></li>
+		                            <li><a href="feature-typograpy.html">Tipograpy</a></li>
+		                            <li><a href="feature-icons.html">Icons</a></li>
+		                            <li><a href="feature-shortcodes.html">Shortcodes</a></li>
+		                            <li>
+		                                <a href="#">Third Level</a>
+		                                <ul>
+		                                    <li><a href="#">menu item</a></li>
+		                                    <li><a href="#">menu item</a></li>
+		                                    <li><a href="#">menu item</a></li>
+		                                </ul>
+		                            </li>
+		                         </ul> -->
+		                    </li>                          
+		                    <li><a href="#">پنل نمایندگی</a></li>
+		                    <li><a href="#">درباره ما</a></li>
+		                </ul>
+		                <!-- End Menu-->
+		              </div>
+		          </div>
+		      </div>
+		  </nav>
+		  <!-- End Nav -->
+
+		  <!-- Slide -->           
+		  <section class="camera_wrap camera_white_skin" id="slide">
+		      <!-- Item Slide -->  
+		      <div data-src="./images/slides/1.jpg">
+		          <div class="overlay-bg animated fadeIn"></div>
+		          <!-- <div class="camera_caption">
+		              <div class="container">
+		                  <div class="row">
+		                      <h1 class="animated bounceInDown delay2">You relax, we care for your information!</h1>
+		                      <p class="animated bounceInDown delay1">We care very well your information, our priority is to ensure the safety of your data! </p>
+		                      <img src="./images/slides/server1.png" alt="" class=" animated bounceInUp delay3">
+		                  </div>
+		              </div>
+		          </div> -->
+		      </div>
+		      <!-- End Item Slide -->  
+
+		      <!-- Item Slide -->  
+		      <div data-src="./images/slides/2.png">
+		          <div class="overlay-bg animated fadeIn"></div>
+		          <!-- <div class="camera_caption">
+		              <div class="container">
+		                  <div class="row">
+		                      <h1 class="animated fadeInDown delay3">NEW VPS  PLANS FOR YOUR BUSINESS</h1>
+		                      <p class="animated fadeInDown delay2">The ultimate in virtual hosting, for peace of mind and optimal efficiency! </p>
+		                      <img src="./images/slides/server1.png" alt="" class=" animated fadeInDown delay1">
+		                  </div>
+		              </div>
+		          </div> -->
+		      </div>
+		      <!-- End Item Slide -->  
+
+		      <!-- Item Slide -->  
+		      <div data-src="./images/slides/3.jpg">
+		          <div class="overlay-bg animated fadeIn"></div>
+		          <!-- <div class="camera_captionb">
+		              <div class="container">
+		                  <div class="row">
+		                     <div class="col-xs-8 col-sm-8 col-md-8">
+		                        <h1 class="animated fadeInDown delay1">The ecology of future servers</h1>
+		                        <p class="animated fadeInUp delay1">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
+		                        <a href="#" class="button animated fadeInDown delay2">Read More</a>
+		                     </div>
+		                     <div class="col-xs-4 col-sm-4 col-md-4">
+		                        <img src="./images/slides/server3.png" alt="" class="animated fadeInRight delay3">
+		                     </div>
+		                  </div>
+		              </div>
+		          </div> -->
+		      </div>
+		      <!-- End Item Slide -->  
+
+		       <!-- Item Slide --> 
+		      <div data-src="./images/slides/4.jpg">
+		         <div class="overlay-bg animated fadeIn"></div>
+		          <!-- <div class="camera_captionc">
+		              <div class="container">
+		                  <div class="row">
+		                     <h1 class="animated fadeInDown delay1">Our goal is to reach the chasm!</h1>
+		                      <p class="animated fadeInUp delay1">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+		                  </div>
+		              </div>
+		          </div> -->
+		      </div>
+		       <!-- End Item Slide --> 
+		  </section>
+		  <!-- End Slide -->             
+		</header>
+		<!-- Header -->
