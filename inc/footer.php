268cdba1d582dcd05f27d831de626b31f05191a9
mojiAm <amjadi.mojtaba@gmail.com>
2014-11-15 12:48:06 +0330

    seprate files

---
+++ inc/footer.php
@@ -0,0 +1,58 @@
+<!-- Footer -->
+  <footer class="footer" id="footer">   
+    <!-- Bottom Footer -->
+    <div class="bottomFooter">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-6 copyrights" style="float:left;text-align:left">
+            <p class="latinfont">
+              All Rights Reserved © SMS 3030  |  Template By 
+              <a href="http://www.mediateq.ir" target="_blank">Mediateq.ir</a>
+            </p>
+          </div><!-- end of copyrights -->
+
+          <div class="col-md-6 footerSocial">
+            <div class="footerSocialWrapper">
+
+              <ul class="bottomSocial socialNav">
+                <li class="facebook"><a href="#"><i class="animated fa fa-facebook"></i></a></li>
+                <li class="twitter"><a href="#"><i class="animated fa fa-twitter"></i></a></li>
+                <li class="rss"><a href="#"><i class="animated fa fa-rss"></i></a></li>
+              </ul><!-- end of bottom social -->
+
+              <!-- <ul class="paymentsNav">
+                <li class="visa">
+                  <a href="#" title="visa"><img alt="visa card" src="images/visa.png" title="visa card"></a>
+                </li>
+                <li class="master">
+                  <a href="#" title="master card"><img alt="master card" src="images/master-card.png" title="master card"></a>
+                </li>
+                <li class="americanExpress">
+                  <a href="#" title="american express"><img alt="american express" src="images/american-express.png" title="american express"></a>
+                </li>
+                <li class="paypal">
+                  <a href="#" title="paypal"><img alt="paypal" src="images/paypal.png" title="paypal"></a>
+                </li>
+              </ul> end of payments nav -->
+            </div><!-- end of footer social wrapper -->
+          </div><!-- end of footer social -->
+        </div><!-- end of row -->
+      </div><!-- end of container -->
+    </div><!-- end of bottom footer -->
+    
+  </footer><!-- end of footer -->
+  
+  
+</div><!-- end of all wrapper -->
+
+  
+<!-- JavaScript Files ================================================== -->
+<script src="./js/compiler.js" type="text/javascript"></script>
+<script src="./js/scripts.js" type="text/javascript"></script>
+
+<!-- BootStrap JavaScript ================================================== -->
+<script src="./js/bootstrap.js" type="text/javascript"></script>  
+  
+</body>
+</html>
