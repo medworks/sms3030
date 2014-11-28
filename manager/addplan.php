603463d4229f152e2eac8b19366f0a4e8aefb9c6
Saeid Hatami <hatami4510@gmail.com>
2014-11-28 20:28:46 +0330

    add code to plans

--- manager/addplan.php
+++ manager/addplan.php
@@ -15,196 +15,45 @@
 		die(); // solve a security bug
 	}
 	$db = Database::GetDatabase();
-	function upload($db,$did,$mode)
-	{
-		if(is_uploaded_file($_FILES['userfile']['tmp_name']) && getimagesize($_FILES['userfile']['tmp_name']) != false)
-		{    
-			$size = getimagesize($_FILES['userfile']['tmp_name']);		
-			$type = $size['mime'];
-			$imgfp = mysql_real_escape_string(file_get_contents($_FILES['userfile']['tmp_name']));
-			//echo $imgfp;
-			$size = $size[3];
-			$name = $_FILES['userfile']['name'];
-			$maxsize = 512000;//512 kb
-			//$db = Database::GetDatabase();
-			//echo $db->cmd;
-			if($_FILES['userfile']['size'] < $maxsize )
-			{    
-				//echo "my1";
-				//tid 1 is for menu pics, 2 for news pics, 3 for maghalat pics
-				if ($mode == "insert")
-				{
-					$fields = array("`tid`","`sid`","`itype`","`img`","`iname`","`isize`");		
-					$values = array("'1'","'{$did}'","'{$type}'","'{$imgfp}'","'{$name}'","'{$size}'");	
-					$db->InsertQuery('pics',$fields,$values);
-				}
-				else
-				{
-				  $imgrow =$db->Select("pics","*","sid='{$did}'");
-				  if ($imgfp != $imgrow["img"])
-				  {
-					$values = array("`tid`"=>"'1'","`sid`"=>"'{$did}'",
-						"`itype`"=>"'{$type}'","`img`"=>"'{$imgfp}'",
-						"`iname`"=>"'{$name}'","`isize`"=>"'{$size}'");
-					$db->UpdateQuery("pics",$values,array("sid='{$did}'"));	
-				  }	
-				}	
-				//echo $db->cmd;
-			}
-			else
-			{        
-				throw new Exception("File Size Error");
-			}
-		}
-		else
-		{		
-			throw new Exception("Unsupported Image Format!");
-		}
-	}	
-	
-	if ($_POST["mark"]=="savedata")
-	{
-		if (isset($_POST["cbsm2"]) and $_POST["cbsm2"]!=0)
-		{
-			$sm = $_POST["cbsm2"];
-		}
-		else
-		if (isset($_POST["cbsm1"]) and $_POST["cbsm1"]!=0)
-		{
-			$sm = $_POST["cbsm1"];
-		}
-		$date = date('Y-m-d H:i:s');
-		$fields = array("`mid`","`smid`","`subject`","`text`","`regdate`","`picid`");		
-		$values = array("'{$_POST[cbmenu]}'","'{$sm}'","'{$_POST[edtsubject]}'","'{$_POST[edttext]}'","'{$date}'","'0'");	
-		if (!$db->InsertQuery('menusubjects',$fields,$values)) 
+
+	if ($_POST["mark"]=="saveplan")
+	{			
+		$fields = array("`name`","`type`","`offer`","`pos`","`params`");		
+		$values = array("'{$_POST[edtname]}'","'{$_POST[type]}'","'{$_POST[offer]}'",
+						"'{$_POST[edtpos]}'","'0'");	
+		if (!$db->InsertQuery('plans',$fields,$values)) 
 		{			
-			header('location:dataentry.php?act=new&msg=2');			
+			header('location:addplan.php?act=new&msg=2');			
 		} 	
 		else 
-		{  
-			if ($_FILES['userfile']['tmp_name']!="")
-			{
-				$did = $db->InsertId();
-				upload($db,$did,"insert");
-				header('location:dataentry.php?act=new&msg=1');
-			}	
+		{  			
+			header('location:addplan.php?act=new&msg=1');			
 		}  		
 	}
 	else
-	if ($_POST["mark"]=="editdata")
-	{		
-		if (isset($_POST["cbsm2"]) and $_POST["cbsm2"]!=0)
-		{
-			$sm = $_POST["cbsm2"];
-		}
-		else
-		if (isset($_POST["cbsm1"]) and $_POST["cbsm1"]!=0)
-		{
-			$sm = $_POST["cbsm1"];
-		}
-		
+	if ($_POST["mark"]=="editplan")
+	{						
 		$values = array("`mid`"=>"'{$_POST[cbmenu]}'","`smid`"=>"'{$sm}'",
 						"`subject`"=>"'{$_POST[edtsubject]}'","`text`"=>"'{$_POST[edttext]}'",
 						"`picid`"=>"'0'");
-        $db->UpdateQuery("menusubjects",$values,array("id='{$_GET[did]}'"));
-		upload($db,$_GET["did"],"edit");	
-		header('location:dataentry.php?act=new&msg=1');
+        $db->UpdateQuery("plans",$values,array("id='{$_GET[pid]}'"));		
+		header('location:addplan.php?act=new&msg=1');
 	}
 	
 	if ($_GET['act']=="new")
 	{
 		$insertoredit = "
 			<button id='submit' type='submit' class='btn btn-default'>ثبت</button>
-			<input type='hidden' name='mark' value='savedata' /> ";
-		$menues = $db->SelectAll("menues","*");	
-		$cbmenu = DbSelectOptionTag("cbmenu",$menues,"name",NULL,NULL,"form-control",NULL,"  منو  ");	
+			<input type='hidden' name='mark' value='saveplan' /> ";		
 	}
 
-	if ($_GET['act']=="view")
-	{
-	    $row=$db->Select("menusubjects","*","id='{$_GET["did"]}'",NULL);
-		
-		$menues = $db->SelectAll("menues","*");	
-		$cbmenu = DbSelectOptionTag("cbmenu",$menues,"name","{$row[mid]}",NULL,"form-control",NULL,"  منو  ");
-		
-		$srow=$db->Select("submenues","*","id='{$row["smid"]}'",NULL);
-		if ($srow["pid"] == 0)	
-		{
-			$m = $srow["mid"];
-			$m1 = $srow["id"];
-			$m2 = 0;
-		}
-		else
-		{			
-			$srow2 = $db->Select("submenues","*","id='{$srow["pid"]}'",NULL);
-			if ($srow2["pid"] == 0)
-			{
-				$m1 = $srow["pid"];
-				$m2 = $srow["id"];
-			}
-			else
-			{
-				$m1 = 0;
-				$m2 = 0;
-			}	
-		}
-		
-		$sm1 = $db->SelectAll("submenues","*","pid = 0");	
-		$cbsm1 = DbSelectOptionTag("cbsm1",$sm1,"name","{$m1}",NULL,"form-control",NULL,"زیر منو");	
 
-		$sm2 = $db->SelectAll("submenues","*","pid <> 0");	
-		$cbsm2 = DbSelectOptionTag("cbsm2",$sm2,"name","{$m2}",NULL,"form-control",NULL,"زیر منو");	
-		
-		$pic = $db->Select("pics","*","sid='{$_GET["did"]}' AND tid = 1",NULL);
-		if (isset($pic))
-		{
-			$imgload = "<img  src='img.php?did={$_GET[did]}&tid=1'  width='200px' height='180px' />";
-		}	
-	}
-	
 	if ($_GET['act']=="edit")
 	{
-	    $row=$db->Select("menusubjects","*","id='{$_GET["did"]}'",NULL);		
+	    $row=$db->Select("plans","*","id='{$_GET["pid"]}'",NULL);		
 		$insertoredit = "
 			<button id='submit' type='submit' class='btn btn-default'>ویرایش</button>
-			<input type='hidden' name='mark' value='editdata' /> ";
-
-		$menues = $db->SelectAll("menues","*");	
-		$cbmenu = DbSelectOptionTag("cbmenu",$menues,"name","{$row[mid]}",NULL,"form-control",NULL,"  منو  ");
-		
-		$srow=$db->Select("submenues","*","id='{$row["smid"]}'",NULL);
-		if ($srow["pid"] == 0)	
-		{
-			$m = $srow["mid"];
-			$m1 = $srow["id"];
-			$m2 = 0;
-		}
-		else
-		{			
-			$srow2 = $db->Select("submenues","*","id='{$srow["pid"]}'",NULL);
-			if ($srow2["pid"] == 0)
-			{
-				$m1 = $srow["pid"];
-				$m2 = $srow["id"];
-			}
-			else
-			{
-				$m1 = $srow["id"];
-				$m2 = $srow2["pid"];
-			}	
-		}
-		
-		$sm1 = $db->SelectAll("submenues","*","pid = 0");	
-		$cbsm1 = DbSelectOptionTag("cbsm1",$sm1,"name","{$m1}",NULL,"form-control",NULL,"زیر منو");	
-
-		$sm2 = $db->SelectAll("submenues","*","pid <> 0");	
-		$cbsm2 = DbSelectOptionTag("cbsm2",$sm2,"name","{$m2}",NULL,"form-control",NULL,"زیر منو");	
-		
-		$pic = $db->Select("pics","*","sid='{$_GET["did"]}' AND tid = 1",NULL);
-		if (isset($pic))
-		{
-			$imgload = "<img  src='img.php?did={$_GET[did]}&tid=1'  width='200px' height='180px' />";
-		}	
+			<input type='hidden' name='mark' value='editplan' /> ";		
 	}
 	
 	
@@ -246,16 +95,16 @@ $html=<<<cd
                                         <input id="edtpos" name="edtpos"  type="text" class="form-control" placeholder="ترتیب" value="{$row['pos']}" />
                                     </div>
                                     <label style="width:90px">
-                                        <input type="checkbox" name="check_out">
+                                        <input type="checkbox" name="offer" value="1">
                                         <i></i> پلن پیشنهادی
                                     </label>
                                     <div class="radio-inline">
                                         <label class="radio-inline">
-                                            <input type="radio" name="optionsRadios2" id="optionsRadios3" value="option3" checked="">
+                                            <input type="radio" name="type" id="optionsRadios3" value="1" checked="">
                                             کاربری
                                         </label>
                                         <label class="radio-inline">
-                                            <input type="radio" name="optionsRadios2" id="optionsRadios4" value="option4">
+                                            <input type="radio" name="type" id="optionsRadios4" value="2">
                                             نمایندگی
                                         </label>
                                     </div>
