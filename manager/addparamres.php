da8e75f25ce7ae0f470747987798f68d694beadb
mojiAm <amjadi.mojtaba@gmail.com>
2014-12-01 10:45:37 +0330

    add plan reseller

--- manager/addparamres.php
+++ manager/addparamres.php
@@ -64,7 +64,7 @@ $html=<<<cd
                 <div class="row">
                     <div class="col-md-12">
                         <!--Top header start-->
-                        <h3 class="ls-top-header">تعریف پارامترهای پلن کاربری</h3>
+                        <h3 class="ls-top-header">تعریف پارامترهای پلن نمایندگی</h3>
                         <!--Top header end -->
                         <!--Top breadcrumb start -->
                         <ol class="breadcrumb">
