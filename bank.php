92542ad6dcc1a1264435325440ed6417d8e21552
Saeid Hatami <hatami4510@gmail.com>
2014-12-03 20:27:56 +0330

    add code to bank page

--- bank.php
+++ bank.php
@@ -1,12 +1,38 @@
 <?php
 	include_once("config.php");
-	include_once("classes/functions.php");
+	include_once("./classes/functions.php");
+  	include_once("./classes/messages.php");
+  	include_once("./classes/session.php");	
+  	include_once("./classes/security.php");
+  	include_once("./classes/database.php");	
+			
+	$db = Database::GetDatabase(); 
   	  	      
-	$Tell_Number = GetSettingValue('Tell_Number',0);
-	$Contact_Email = GetSettingValue('Contact_Email',0);	
-	$About_System = GetSettingValue('About_System',0);
-
-$ahtml2 =<<<cd
+	$banks=$db->SelectAll("bankinfo","*");
+$lstbank=<<<cd
+		<table>
+			<tr>
+				<th style="width:150px">نام بانک</th>
+				<th style="width:100px">صاحب حساب</th>
+				<th style="width:100px">شماره حساب</th>
+				<th style="width:100px">شماره کارت</th>
+			</tr>
+cd;
+	for($i=0;$i<count($banks);$i++)
+	{
+$lstbank.=<<<cd
+			<tr>
+				<td>{$banks[$i]['name']}</td>
+				<td>{$banks[$i]['owner']}</td>	
+				<td>{$banks[$i]['accno']}</td>	
+				<td>{$banks[$i]['cardno']}</td>					
+			</tr>			
+cd;
+	}
+$lstbank.=<<<cd
+		</table>		
+cd;
+$bhtml2 =<<<cd
     <!-- Page Info -->
     <div class="pageInfo">
       <div class="cover"></div>
@@ -22,8 +48,7 @@ $ahtml2 =<<<cd
               </li>
               <li class="active"><a href="about-us.html">شماره حساب</a></li>
             </ol><!-- end of breadcrumb -->
-          </div><!-- end of col-md-8 -->
-
+          </div><!-- end of col-md-8 -->		
         </div><!-- end of row -->
       </div><!-- end of container -->
     </div><!-- end of page info -->
@@ -36,6 +61,7 @@ $ahtml2 =<<<cd
         <div class="row">          
           <div class="col-md-12">
             <p style="font-size:23px;text-align:justify;font-weight:normal;line-height:35px;padding:0 35px;color:#000">{$About_System}</p>
+			{$lstbank}
           </div><!-- end of col-md-12 -->
         </div><!-- end of row-->
       </div><!-- end of container -->
@@ -47,7 +73,7 @@ cd;
   include_once('./inc/header.php');
   include_once('./inc/pageheader.php');
   include_once('./inc/menu.php');
-  echo $ahtml2;
+  echo $bhtml2;
   include_once('./inc/clients.php');
   include_once('./inc/footer.php');
 ?>
