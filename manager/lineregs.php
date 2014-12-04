a376984bae01ce742e5c9458e7ac21d8580c1f82
Saeid Hatami <hatami4510@gmail.com>
2014-12-04 23:30:50 +0330

    add page for confirming line registers

---
+++ manager/lineregs.php
@@ -0,0 +1,132 @@
+<?php
+	include_once("../config.php");
+	include_once("../classes/functions.php");
+  	include_once("../classes/messages.php");
+  	include_once("../classes/session.php");	
+  	include_once("../classes/security.php");
+  	include_once("../classes/database.php");	
+	include_once("../classes/login.php");
+    include_once("../lib/persiandate.php"); 
+		
+	$login = Login::GetLogin();
+    if (!$login->IsLogged())
+	{
+		header("Location: ../index.php");
+		die(); // solve a security bug
+	} 
+	
+	$db = Database::GetDatabase(); 
+	
+	if (isset($_GET["act"]) and $_GET["act"]=="view")
+	{
+		$row = $db->Select("linenumbers","*","id ={$_GET['lid']}");
+		$regdate = ToJalali($row["regdate"],"Y/m/d H:i");
+	}
+	
+	if ((isset($_POST["mark"]) and $_POST["mark"]=="confirm"))
+	{
+		$values = array("`confirm`"=>"'1'");
+		$db->UpdateQuery("linenumbers",$values,array("id='{$_GET[lid]}'"));		
+		header('location:lineordered.php?act=new');
+	}
+$html=<<<cd
+<section id="min-wrapper">
+        <div id="main-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!--Top header start-->
+                        <h3 class="ls-top-header">مشاهده اطلاعات ثبت نام کنندگان</h3>
+                        <!--Top header end -->
+                        <!--Top breadcrumb start -->
+						تاریخ ثبت نام {$regdate}
+                        <ol class="breadcrumb">
+                            <li><a href="admin.php"><i class="fa fa-home"></i></a></li>
+                            <li class="active">مشاهده اطلاعات</li>							
+                        </ol>
+                        <!--Top breadcrumb start -->
+                    </div>
+                </div>
+                <!-- Main Content Element  Start-->
+                <form id="frmagents" name="frmagents"  action="" method="post" class="form-inline ls_form" role="form">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">نام و نام خانوادگی</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edtname" name="edtname" type="text" class="form-control" value="{$row['name']}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">نام شرکت</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edtcompany" name="edtcompany" type="text" class="form-control" value="{$row['company']}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">ایمیل</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edtemail" name="edtemail" type="text" class="form-control" value="{$row['email']}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">نام کاربری</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edtusername" name="edtusername" type="text" class="form-control" value="{$row['username']}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                                                                                                                   
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">تایید اطلاعات</h3>
+                                </div>
+                                <div class="panel-body">               
+                                    <button id="submit" type="submit" class="btn btn-default">تایید</button>
+                                    <input type="hidden" name="mark" value="confirm"> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- Main Content Element  End-->
+            </div>
+        </div>
+    </section>
+
+cd;
+    include_once("./inc/header.php");
+    echo $html;
+    include_once("./inc/footer.php");
+?>
