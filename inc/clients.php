5da12b8c9c833debf4b6df4ded76969101af8da7
Saeid Hatami <hatami4510@gmail.com>
2014-12-06 01:58:49 +0330

    add clients page codes

--- inc/clients.php
+++ inc/clients.php
@@ -1,4 +1,14 @@
-<!-- Clients -->
+<?php
+	include_once("./config.php");
+	include_once("./classes/functions.php");
+  	include_once("./classes/messages.php");
+  	include_once("./classes/session.php");	
+  	include_once("./classes/security.php");
+  	include_once("./classes/database.php");	
+	
+	$db = Database::GetDatabase(); 
+	$rows = $db->SelectAll("clients","*");
+$chtml=<<<cd
 <section class="clients section mainSection scrollAnchor lightSection" id="clients">
   <div class="sectionWrapper">
     <div class="container">
@@ -12,46 +22,18 @@
       </div><!-- end of row -->
       <div class="row">
         <div class="clientsCarousel owl-carousel clientsGallary">
+cd;
+for($i = 0; $i < Count($rows); $i++)
+{	
+$chtml.=<<<cd
           <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
             <a class="singleClient" href="#" title="client">
-              <img alt="client 1" src="images/clients/1.png" title="client">
+			  <img src="./manager/img.php?did={$rows[$i]["id"]}&type=client" alt="{$rows[$i]['subject']}" title="{$rows[$i]['subject']}" />              
             </a><!-- end of single client -->
           </div><!-- end of single client wrapper -->
-          <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
-            <a class="singleClient" href="#" title="client">
-              <img alt="client 2" src="images/clients/2.png" title="client">
-            </a><!-- end of single client -->
-          </div><!-- end of single client wrapper -->
-          <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
-            <a class="singleClient" href="#" title="client">
-              <img alt="client 3" src="images/clients/3.png" title="client">
-            </a><!-- end of single client -->
-          </div><!-- end of single client wrapper -->
-          <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
-            <a class="singleClient" href="#" title="client">
-              <img alt="client 4" src="images/clients/4.png" title="client">
-            </a><!-- end of single client -->
-          </div><!-- end of single client wrapper -->
-          <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
-            <a class="singleClient" href="#" title="client">
-              <img alt="client 5" src="images/clients/5.png" title="client">
-            </a><!-- end of single client -->
-          </div><!-- end of single client wrapper -->
-          <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
-            <a class="singleClient" href="#" title="client">
-              <img alt="client 6" src="images/clients/6.png" title="client">
-            </a><!-- end of single client -->
-          </div><!-- end of single client wrapper -->
-          <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
-            <a class="singleClient" href="#" title="client">
-              <img alt="client 7" src="images/clients/7.png" title="client">
-            </a><!-- end of single client -->
-          </div><!-- end of single client wrapper -->
-          <div class="col-md-2 col-sm-4 item client singleClientsWrapper">
-            <a class="singleClient" href="#" title="client">
-              <img alt="client 8" src="images/clients/8.png" title="client">
-            </a><!-- end of single client -->
-          </div><!-- end of single client wrapper -->
+cd;
+}
+ $chtml.=<<<cd
           <!-- end of single client wrapper -->
         </div><!-- end of clients gallary -->
       </div><!-- end of row -->
@@ -59,3 +41,7 @@
   </div>
 </section>
 <!-- end clients section -->
+cd;
+
+echo $chtml;
+?>
