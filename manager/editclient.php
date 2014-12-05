a895a8c17f40c4f9cc9d2dea2799f24176424796
Saeid Hatami <hatami4510@gmail.com>
2014-12-06 01:39:36 +0330

    add new file for edit clients

---
+++ manager/editclient.php
@@ -0,0 +1,133 @@
+<?php
+	include_once("../config.php");
+	include_once("../classes/functions.php");
+  	include_once("../classes/messages.php");
+  	include_once("../classes/session.php");	
+  	include_once("../classes/security.php");
+  	include_once("../classes/database.php");	
+	include_once("../classes/login.php");
+    include_once("../lib/persiandate.php"); 
+	include_once("../lib/Zebra_Pagination.php"); 
+	
+	
+	$login = Login::GetLogin();
+    if (!$login->IsLogged())
+	{
+		header("Location: ../index.php");
+		die(); // solve a security bug
+	} 
+	$db = Database::GetDatabase(); 
+	if ($_GET['act']=="del")
+	{
+		$db->Delete("clients"," id ",$_GET["did"]);		
+		header('location:editclients.php?act=edit');	
+	}		
+    
+$html.=<<<cd
+    <!--Page main section start-->
+    <section id="min-wrapper">
+        <div id="main-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!--Top header start-->
+                        <h3 class="ls-top-header">ویرایش مشتری</h3>
+                        <!--Top header end -->
+                        <!--Top breadcrumb start -->
+                        <ol class="breadcrumb">
+                            <li><a href="admin.php"><i class="fa fa-home"></i></a></li>
+                            <li class="active">ویرایش مشتری</li>
+                        </ol>
+                        <!--Top breadcrumb start -->
+                    </div>
+                </div>
+                <!-- Main Content Element  Start-->
+                <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">مشتری های ثبت شده</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <!--Table Wrapper Start-->
+                                    <div class="table-responsive ls-table">
+                                        <div id="ls-editable-table_filter" class="dataTables_filter">
+                                            <label>جستجو:<input type="search" class="" aria-controls="ls-editable-table"></label>
+                                        </div>
+                                        <table class="table table-bordered table-striped">
+                                            <thead>
+                                            <tr>
+											<th>#</th>												
+                                                <th>عنوان</th>                                                
+                                                <th>تصویر</th>
+                                                <th class="text-center">عملیات</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+cd;
+
+	$records_per_page = 10;
+	$pagination = new Zebra_Pagination();
+
+	$pagination->navigation_position("right");
+
+	$reccount = $db->CountAll("clients");
+	$pagination->records($reccount); 
+	
+    $pagination->records_per_page($records_per_page);	
+
+$rows = $db->SelectAll(
+				"clients",
+				"*",
+				NULL,
+				"id ASC",
+				($pagination->get_page() - 1) * $records_per_page,
+				$records_per_page);
+				
+	
+$vals = array();
+for($i = 0; $i < Count($rows); $i++)
+{
+$rownumber = $i+1;
+$rows[$i]["subject"] =(mb_strlen($rows[$i]["subject"])>20)?mb_substr($rows[$i]["subject"],0,20,"UTF-8")."...":$rows[$i]["subject"];
+
+$html.=<<<cd
+
+                                                
+                                            <tr>
+                                                <td>{$rownumber}</td>									
+                                                <td>{$rows[$i]["subject"]}</td>                                               
+                                                <td>
+                                                    <img src="img.php?did={$rows[$i]["id"]}&type=client" width="50px" height="50px" /> 
+                                                </td>
+                                                <td class="text-center">
+												<a href="?act=del&did={$rows[$i]["id"]}"  >												
+                                                    <button class="btn btn-xs btn-danger" title="پاک کردن"><i class="fa fa-minus"></i></button>
+												</a>	
+                                                </td>
+                                            </tr>
+cd;
+}
+
+	$pgcodes = $pagination->render(true);
+	//var_dump($pagination);
+$html.=<<<cd
+                                            </tbody>
+                                        </table>
+                                    </div>
+									{$pgcodes}
+                                    <!--Table Wrapper Finish-->                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>           
+                <!-- Main Content Element  End-->
+            </div>
+        </div>
+    </section>
+    <!--Page main section end -->
+cd;
+	include_once("./inc/header.php");
+	echo $html;
+    include_once("./inc/footer.php");
+?>
