68180af72fe47d9e5586a7821bd2b1b0a233a72c
Saeid Hatami <hatami4510@gmail.com>
2014-12-05 22:46:00 +0330

    add code to single plan

--- single-plan.php
+++ single-plan.php
@@ -1,9 +1,19 @@
 <?php
-  include_once('./inc/header.php');
-  include_once('./inc/pageheader.php');
-  include_once('./inc/menu.php');
-?>
-    <!-- Page Info -->
+    include_once("./config.php");
+	include_once("./classes/functions.php");
+  	include_once("./classes/messages.php");
+  	include_once("./classes/session.php");	
+  	include_once("./classes/security.php");
+  	include_once("./classes/database.php");	
+	
+	$db = Database::GetDatabase(); 
+	$plans = $db->SelectAll("plans","*","type = 1","pos ASC");
+	$params = $db->SelectAll("params","*",NULL,"pos ASC");
+	$PlanDescribe = GetSettingValue('PlanDescribe',0);
+				
+	
+$splan=<<<cd
+ <!-- Page Info -->
     <div class="pageInfo">
       <div class="cover"></div>
       <div class="container">
@@ -34,31 +44,45 @@
                 <thead>
                   <tr>
                     <th>امکانات / سطح دسترسی</th>
-                    <th>پنل پایه / B</th>
-                    <th>پنل تبلیغاتی / A</th>
-                    <th>پنل سازمانی / A+</th>
-                    <th>پنل حرفه ای / AS</th>
-                  </tr>
+cd;
+
+for($i = 0; $i < Count($plans); $i++)
+{
+$splan.=<<<cd
+                    <th>{$plans[$i]["name"]}</th>
+cd;
+}
+$splan.=<<<cd
+                </tr>
                 </thead>
-
                 <tbody>
-
+cd;
+for($i = 0; $i < Count($params); $i++)
+{
+$splan.=<<<cd
                   <tr>
-                    <td>ارسال پیامک به صورت تکی و گروهی</td>
-                    <td><img src="./images/tik.png" alt="" width="25" height="25"/></td>
-                    <td><img src="./images/cross.png" alt="" width="25" height="25"/></td>
-                    <td><img src="./images/tik.png" alt="" width="25" height="25"/></td>
-                    <td><img src="./images/cross.png" alt="" width="25" height="25"/></td>
+                    <td>{$params[$i]["name"]}</td>
+cd;
+for($j = 0; $j < Count($plans); $j++)
+{
+	$param = explode(",",$plans[$j]["params"]);
+	if (in_array($params[$i]["name"],$param))
+	{
+		$img =" <img src='./images/tik.png' alt='' width='25' height='25' />"; 
+	}
+	else
+	{
+		$img =" <img src='./images/cross.png' alt='' width='25' height='25' />"; 
+	}
+$splan.=<<<cd
+                    <td>{$img}</td>
+cd;
+}
+$splan.=<<<cd
                   </tr>
-
-                  <tr>
-                    <td>ارسال پیامک به صورت تکی و گروهی</td>
-                    <td><img src="./images/tik.png" alt="" width="25" height="25"/></td>
-                    <td><img src="./images/cross.png" alt="" width="25" height="25"/></td>
-                    <td><img src="./images/tik.png" alt="" width="25" height="25"/></td>
-                    <td><img src="./images/cross.png" alt="" width="25" height="25"/></td>
-                  </tr>
-
+cd;
+}
+$splan.=<<<cd
                   <tr>
                     <td>سفارش/قیمت (ریال)</td>
                     <td><a href="http://panel.sms3030.ir" class="generalLink order" style="font-size:18px">80000</a></td>
@@ -69,7 +93,7 @@
 
                 </tbody>
               </table>
-              <p class="detail" style="padding: 20px 10px; color: #cd2f2e;">1- توضیحات تکمیلی....</p>
+              <p class="detail" style="padding: 20px 10px; color: #cd2f2e;">{$PlanDescribe}</p>
             <div id="ascrail2002" class="nicescroll-rails" style="width: 6px; z-index: 99999999999; position: absolute; top: 0px; right: 0px; opacity: 1; cursor: default; display: none; height: 718px;"><div style="position: relative; top: 0px; float: right; width: 6px; border: 0px; border-radius: 3px; height: 0px; background-color: rgb(204, 204, 204); background-clip: padding-box;"></div></div><div id="ascrail2002-hr" class="nicescroll-rails" style="height: 6px; z-index: 99999999999; position: absolute; left: 0px; bottom: 0px; opacity: 1; cursor: default; display: none; width: 1168px;"><div style="position: relative; top: 0px; height: 6px; border: 0px; border-radius: 3px; width: 0px; background-color: rgb(204, 204, 204); background-clip: padding-box;"></div></div></div><!-- end of table wrapper -->
 
           </div><!-- end of col-md-12 -->
@@ -79,8 +103,12 @@
       </div><!-- end of container -->
     </div><!-- end of section wrapper -->
   </section>
+cd;
 
-<?php
+  include_once('./inc/header.php');
+  include_once('./inc/pageheader.php');
+  include_once('./inc/menu.php');
+  echo $splan;
   include_once('./inc/clients.php');
   include_once('./inc/footer.php');
 ?>
