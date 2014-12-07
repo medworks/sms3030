7ae5531b64d9615d381c0b91906e1ecce69d5cf4
mojiAm <amjadi.mojtaba@gmail.com>
2014-12-07 19:23:00 +0330

    fix tel

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $slide.=<<<cd
             <ul>
               <li class="tele">
                 Tel: 
-                <a href="javascript:void();" class="latinfont ltr" style="display:inline-block;letter-spacing:2px">+98 {$Tell_Number}</a>
+                <a href="javascript:void();" class="latinfont ltr" style="display:inline-block;letter-spacing:2px"> {$Tell_Number}</a>
               </li>
               <li class="mail">
                 Email: 
