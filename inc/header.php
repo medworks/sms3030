14972697628516d9fc9ea901f78a8c6b1092d927
mojiAm <amjadi.mojtaba@gmail.com>
2014-12-20 17:09:00 +0330

    add analysis

--- inc/header.php
+++ inc/header.php
@@ -58,6 +58,21 @@
 <script type="text/javascript" src="./js/jquery-migrate-1.2.1.min.js"></script>
 
 <script type="text/javascript" src="./js/jquery-ui.js"></script>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.sms3030.ir/analys/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="http://www.sms3030.ir/analys/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 
 
 <!--[if lt IE 9]><!-->
