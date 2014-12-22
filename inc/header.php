b5681d141b71798022d44f86adfc14be3bb3be3f
mojiAm <amjadi.mojtaba@gmail.com>
2014-12-22 14:55:08 +0330

    change piwiki

--- inc/header.php
+++ inc/header.php
@@ -48,14 +48,14 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.sms3030.ir/analys/";
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://analys.sms3030.ir/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 1]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
     g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="http://www.sms3030.ir/analys/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="http://analys.sms3030.ir/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
  
 <!-- fav icon -->
